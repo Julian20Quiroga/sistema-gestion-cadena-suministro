3ad5df09d4906711e1070270108c84543dab03aa
Julian20Quiroga <103070330+Julian20Quiroga@users.noreply.github.com>
2026-09-08 20:09:08 -0500

    Implementacion de JWT en la autenticacion y autorizacion para el manejo de sesion del usuario. Proximamente se aplicara en los respectivos endpoints

---
+++ backend/sistema_cadena_suministro/src/main/java/com/logisync/sistema_cadena_suministro/infrastructure/security/JwtService.java
@@ -0,0 +1,65 @@
+package com.logisync.sistema_cadena_suministro.infrastructure.security;
+
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+
+@Component
+public class JwtService {
+
+    private final SecretKey key;
+    private final long expiration = 3600000;
+
+    public JwtService() {
+        this.key = Jwts.SIG.HS256.key().build();
+    }
+
+    public String generateToken(String username, String role) {
+        return Jwts.builder()
+                .subject(username)
+                .claim("role", role)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public String extractRole(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("role", String.class);
+    }
+
+    public boolean isTokenValid(String token) {
+
+        try {
+            Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token);
+
+            return true;
+
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+}
